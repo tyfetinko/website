8fc7bb6fc8f2034eeade0e0be22e4504279f54a6
Ty Fetinko <ty667216@dal.ca>
2022-10-26 18:56:44 -0300

    lets try this

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
         </button>
       </div>
       <div className="links">
-        <Link to="/"> Home </Link>
+        <Link to="/website"> Home </Link>
         <Link to="/skills" >Skills</Link>
         <Link to="/projects"> Projects </Link>
         <Link to="/experience"> Experience </Link>
