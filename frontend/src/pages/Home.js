47e51ebe967499c5709d6e4ae7194bfc73a01fa7
Ty Fetinko <ty667216@dal.ca>
2022-11-01 00:28:55 -0300

    period is bothering me

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, my name is Ty </h2>
+        <h2> Hi, my name is Ty.</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
         </div>
